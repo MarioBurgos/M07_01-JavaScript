a9484bd01a38f4b84cc4cbf1731faf3262874975
Mario Burgos <mario.burpi@gmail.com>
2021-10-08 09:58:44 +0200

    Interfaz de la calculadora

---
+++ Nivel 3/calculadora.js
@@ -0,0 +1,12 @@
+var value1;
+var value2;
+var operator;
+var res;
+
+function getBtnValue(value) {
+
+}
+
+function displayValue(value) {
+    document.getElementsByClassName("display").innerText = value;
+}
