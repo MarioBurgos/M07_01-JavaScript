fdad7731077b4813200246ff391e2a878c6bdf6f
Mario Burgos <mario.burpi@gmail.com>
2021-10-11 11:04:44 +0200

    Añadidos dos botones C y AC para borrar el ultimo input y toda la memoria

--- Nivel 3/calculadora.js
+++ Nivel 3/calculadora.js
@@ -1,5 +1,13 @@
 var value = '';
-var operation = [];
+var memory = '';
+var smallDisplay;
+var bigDisplay;
+
+
+function init() {
+    smallDisplay = document.getElementById("small-display");
+    bigDisplay = document.getElementById("big-display");
+}
 
 /** 
  * Recibe el valor del botón que se acaba de pulsar y lo concatena a una variable.
@@ -9,27 +17,31 @@ var operation = [];
 function displayOperation(value) {
     // siempre se pinta en el small-display
     // se controla si es el primer numero o si ya habia uno printado 
-    if (document.getElementById("small-display").innerText != document.getElementById("big-display").innerText) {
-        document.getElementById("small-display").innerText += value;
-        operation.push(value);
-    } else {
-        document.getElementById("small-display").innerText = value;
-
-    }
-
-    console.log('value is ' + value);
+    smallDisplay.innerText += value;
+    // console.log('value is ' + value);
 }
 
 /** Calcula la operación y pinta su resultado en el big-display y small-display  */
 function displayResult() {
-    var operation = document.getElementById("small-display").textContent;
-    result = eval(operation); //  Cuidado con esta función `eval(string)` pues reduce la seguridad de la aplicación.  
-    // TODO calcular el resultado de la operación mediante regex y split.
-    document.getElementById("big-display").innerText = result;
-    document.getElementById("small-display").innerText = result;
-
+    var operation = smallDisplay.textContent;
+    result = eval(operation);
+    smallDisplay.innerText = result;
+    bigDisplay.innerText = result;
 }
 
+/** Elimina el ultimo numero del small-display */
+function deleteInput() {
+    var operation = smallDisplay.textContent;
+    operation = operation.slice(0, operation.length - 1);
+    console.log(operation.charAt(operation.length - 1));
+    smallDisplay.innerText = operation;
+}
+
+/** */
+function resetOperation() {
+    smallDisplay.innerText = '0';
+    bigDisplay.innerText = 0
+}
 
 /**
  * Enseña alertas en la página.
